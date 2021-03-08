e80c9d23d9b7c17e10a42b0f35e45805b1d83313
whw-git <1037883255@qq.com>
2021-03-09 00:56:12 +0800

    实现文件的创建功能

--- Pack.c
+++ Pack.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 #include<string.h>
 #include<malloc.h>
+#include <direct.h>  
 
 #define FILE_TYPE_LEN 10
 #define MAX_FILE_TLV_SIZE 10
 #define BUFFER_SIZE 10*1024*1024
 /* 文件信息，包含文件名和文件内容两部分 */
 #define FILE_INFO_NUM 2
-#define WRITE_FILE_PATH "d:\\output\\output.dat"
+#define WRITE_FILE_PATH "\\output\\output.dat"
+#define MAX_PATH_SIZE 1024
+
+/* 创建文件夹 */
+#define MKDIR(folderName)\
+if (_access(folderName, 0) == -1) { \
+    _mkdir(folderName);             \
+}                                                                  
+
 
 
 #define U32 unsigned int
@@ -36,6 +45,51 @@ typedef struct fileTlv
     FileInfo *fileValue;
 }FileTlv;
 
+/* 创建绝对路径的文件 */
+U32 MakeFile(U8 *path){
+    U8 filePath[MAX_PATH_SIZE];
+    U8 dirNum = 0;
+    U8 index;
+    U8 dirName[20][50];
+    U8 *rootDir;
+    U8 *p;
+    U8 word[] = "\\";
+
+    strcpy(filePath,path);
+
+    /* strtok返回的是拆分的地址,第一二个元素地址相连
+     *如果使用strcat追加的字符串，会把第二个元素覆盖
+     *因此需要重新申请地址，用strcpy进行保存
+    */
+	rootDir = strtok(filePath, word);
+    /* p和rootDir指向的地址相连 */
+    p = strtok(NULL, word);
+	while(p) {
+        strcpy(dirName[dirNum],p);
+		dirNum++;
+		// 再次调用分割时指针要变为NULL, 也就是这里的第一个参数，分割的字符串还是str
+		// 第二个参数要和第一次调用时的分割符保持一致
+		p = strtok(NULL, word);
+	}
+    for (index = 0; index < dirNum - 1; index++) {
+        strcat(rootDir, "\\");
+        strcat(rootDir, dirName[index]);
+        MKDIR(rootDir);
+    }
+    FILE *fileWrite = fopen(path,"wb");
+    fclose(fileWrite);
+}
+
+/* 在当前目录下创建文件，并传出地址 */
+U32 MakeFileInCurrDir(U8 *filePath, U8 **resultPath){
+    /* 获取当前路径 */
+    U8 path[MAX_PATH_SIZE];
+    _getcwd(path,MAX_PATH_SIZE);
+    strcat(path,filePath);
+    MakeFile(path);
+    *resultPath = (char *)malloc(MAX_PATH_SIZE);
+    strcpy(*resultPath,path);
+}
 U32 GetFileType(U8 *filePath, U32 fileNameSize, U32 *fileType){
     U8 *pType;
     U8 i;
@@ -118,6 +172,7 @@ U32 PackBuffer(FileTlv *fileTlvs, U32 *fileNum) {
     U32 fileIndex = 0;
     int fileInfoIndex = 0;
     U32 fileValueIndex = 0;
+    U8 *outPutPath;
 
     packSize += (*fileNum) * (2 * sizeof(U32));
     for(fileIndex = 0; fileIndex < *fileNum; fileIndex++) {
@@ -162,16 +217,16 @@ U32 PackBuffer(FileTlv *fileTlvs, U32 *fileNum) {
             }
         }
     }
-    FILE *fileWrite = fopen(WRITE_FILE_PATH,"wb");
+    /* 在当前目录下创建文件，入参为相对路径 */
+    MakeFileInCurrDir(WRITE_FILE_PATH,&outPutPath);
+    FILE *fileWrite = fopen(outPutPath,"wb");
     //写文件
     fwrite(fileHead,sizeof(U8),sizeof(U8) * packSize + sizeof(U32), fileWrite);
-
     fclose(fileWrite);
 }
 
  
 U32 UnPackFile(U8 *filePath, FileTlv** fileTlvs,U32 *fileNum ) {
-    
     U32 *pointU32;
     U8 *pointCurrent;
     U32 fileIndex = 0;
@@ -181,7 +236,6 @@ U32 UnPackFile(U8 *filePath, FileTlv** fileTlvs,U32 *fileNum ) {
     U8 *fileInfoValue;
     FILE *fileRead = fopen(filePath,"rb");
     
-
     fseek(fileRead, 0, SEEK_END);       
 	U64 tempFileSize = ftell (fileRead);
 
@@ -199,11 +253,7 @@ U32 UnPackFile(U8 *filePath, FileTlv** fileTlvs,U32 *fileNum ) {
     pointCurrent = (U8 *) (void *)pointU32;
     /* 申请文件Tlvs */
     (*fileTlvs) = (FileTlv *) malloc(sizeof(FileTlv) * (*fileNum));
-    if ((*fileTlvs) == NULL)
-    {
-        printf("fuck\n");
-    }
-    
+
     /* 第一层，pck的所有信息，包含所有文件 */
     for(fileIndex = 0; fileIndex < *fileNum; fileIndex++) {
 
@@ -242,12 +292,11 @@ U32 UnPackFile(U8 *filePath, FileTlv** fileTlvs,U32 *fileNum ) {
             }
         }
     }
-    
-
     //fileBuffer = tempFileBuffer;
     return 0;
 }
 
+/* 释放TLV */
 U32 FreeTlv(FileTlv *fileTlvs, U32 *fileNum) {
     U32 fileIndex;
     U32 fileInfoIndex;
@@ -259,6 +308,46 @@ U32 FreeTlv(FileTlv *fileTlvs, U32 *fileNum) {
     }
 }
 
+/* 将tlv写入文件中 */
+// U32 WriteFile(FileTlv *fileTlvs, U32 *fileNum) {
+
+//     U8 *fileBuffer = (U8 *)malloc(BUFFER_SIZE);
+//     FileInfo *fileInfo = (FileInfo *) malloc(sizeof(FileInfo) * 2);
+//     U32 fileType;
+//     U32 fileLen = 0;
+//     U8 currentChar;
+//     GetFileType(filePath, fileNameSize, &fileType);
+    
+//     /* 填充文件名 */
+//     fileInfo[0].fileInfoType = 0;
+//     fileInfo[0].fileInfoLen = fileNameSize;
+//     fileInfo[0].fileInfoValue = filePath;
+
+//     /* 填充文件信息 */
+//     fileInfo[1].fileInfoType = 1;
+//     //读文件到缓冲区
+//     FILE *fileRead = fopen(filePath,"r+");
+//     U8 *pFile=fileBuffer;
+//     currentChar = fgetc(fileRead);
+//     while (!feof(fileRead))
+//     {
+//         *pFile = currentChar;
+//         pFile++;
+//         fileLen++;
+//         currentChar = fgetc(fileRead);
+//     }
+//     pFile = NULL;
+//     fclose(fileRead);
+//     fileInfo[1].fileInfoLen = fileLen;
+//     fileInfo[1].fileInfoValue = fileBuffer;
+    
+//     fileTlvs[*fileNum].fileType = fileType;
+//     fileTlvs[*fileNum].fileLen = 4 * sizeof(U32) + fileInfo[0].fileInfoLen + fileInfo[1].fileInfoLen;
+//     fileTlvs[*fileNum].fileValue =  fileInfo;
+//     *fileNum = *fileNum + 1;
+//     return 0;
+// }
+
 int main(){
     U8 *fileBuffer;
     U64 fileSize = 0;
@@ -268,6 +357,10 @@ int main(){
     U8 path1[] = "d:\\cc.txt";
     U8 path2[] = "d:\\d.txt";
     U8 path3[] = "d:\\map.mp3";
+    U8 *resultPath;
+    MakeFileInCurrDir("\\AA\\BB\\dd\\d.txt",&resultPath);
+
+
     /* 最多保存10个文件 */
     FileTlv *fileTlvs = (FileTlv *)malloc(sizeof(FileTlv) * MAX_FILE_TLV_SIZE);
     FileTlv *unPackFileTlvs = NULL;
@@ -279,14 +372,14 @@ int main(){
     FreeTlv(fileTlvs, &fileNum);
     UnPackFile("D:\\output\\output.dat", &unPackFileTlvs, &unPackFileNum);
 
-    printf("======================%d========================\n",unPackFileNum);
-    if(unPackFileTlvs == NULL)
-        printf("FUCK");
     FilesInfoShow(unPackFileTlvs, &unPackFileNum);
+
+    //todo
+    //WriteFile(unPackFileTlvs, &unPackFileNum);
     //FilesInfoShow(unPackFileTlvs, &unPackFileNum);
     //type = (U32 *) fileBuffer;
 
-    //printf("fileSize = %d", type);
     system("pause");
 
 }
+
